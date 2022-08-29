00bc6270e2b349ba5320234a5735398baaddf528
toxicplayorg <96045370+toxicplayorg@users.noreply.github.com>
2022-08-29 17:48:00 +0200

    Delete library directory

--- library/js/hidefooterinhambmenu.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $(".container .navbar .icon-bar").click(function() {
-    $(".footer").hide();
-  });
-  $(".container .navbar ul .close").click(function() {
-    $(".footer").show();
-  });
-});
