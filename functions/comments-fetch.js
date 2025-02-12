eb368d5c9a7962ef6f0d94bb07c22f10aefcd627
toxicplayorg <96045370+toxicplayorg@users.noreply.github.com>
2025-02-12 01:32:53 +0100

    upload

--- functions/comments-fetch.js
+++ functions/comments-fetch.js
@@ -1,21 +1,20 @@
-exports.handler = async function (event, context) {
+exports.handler = async function(event, context) {
     const discussionId = event.queryStringParameters.id;
     const url = `https://toxicplay.freeflarum.com/api/discussions/${discussionId}`;
 
     try {
-        // Use dynamic import for 'node-fetch'
-        const fetch = await import("node-fetch").then((module) => module.default);
+        const fetch = await import('node-fetch').then(module => module.default);
         const response = await fetch(url);
         const data = await response.json();
 
         return {
             statusCode: 200,
-            body: JSON.stringify(data)
+            body: JSON.stringify(data),
         };
     } catch (error) {
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: error.message })
+            body: JSON.stringify({ error: error.message }),
         };
     }
 };
