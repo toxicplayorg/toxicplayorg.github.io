06a031294368ab5b230060a9bf5e5804f8956fcb
toxicplayorg <96045370+toxicplayorg@users.noreply.github.com>
2025-02-12 01:46:52 +0100

    upload12022025

--- node_modules/library/js/hidediscordinhambmenu.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $(".container .navbar .icon-bar").click(function() {
-    $(".discord-section").hide();
-  });
-  $(".container .navbar ul .close").click(function() {
-    $(".discord-section").show();
-  });
-});
--- node_modules/library/js/paginationcopy.js
+++
@@ -1,3 +0,0 @@
-var paginationClone = $("#Pagination1 > *").clone(true);
-$("#Pagination2").empty();
-paginationClone.appendTo("#Pagination2");
